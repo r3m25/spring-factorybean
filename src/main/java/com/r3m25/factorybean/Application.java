88eb614c60b8e9f798b2004b9b3ab659890bd752
Rubén Marcos Morales Medina <rm.morales22@gmail.com>
2020-07-18 16:17:07 -0400

    add base project

---
+++ src/main/java/com/r3m25/factorybean/Application.java
@@ -0,0 +1,8 @@
+package com.r3m25.factorybean;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){}
+}
